bf5be8fab2847a32824c05692111d397ade7b711
Tanner Haberl <thaberl13@gmail.com>
2020-09-16 16:17:24 +0900

    made main logo clickable to direct back to main page

--- src/App.js
+++ src/App.js
@@ -15,10 +15,15 @@ function currentViewHandler() {
   }
 }
 
+function returnHomeHandler() {
+  if(currentView) {
+    setCurrentView(false)
+  }
+}
 
   return (
     <div className="App">
-     <img className="header-logo" src="https://i.ibb.co/vd1XGDj/movie-list-logo-png.png" alt="movie-dpng"/>
+     <img onClick={returnHomeHandler} className="header-logo" src="https://i.ibb.co/vd1XGDj/movie-list-logo-png.png" alt="movie-dpng"/>
       {/* <input type="button" onClick={currentViewHandler} value="Watch List"/> */}
       {currentView ? <WatchList setCurrentView={setCurrentView} currentViewHandler={currentViewHandler}/> : <Search WatchList={WatchList} currentViewHandler={currentViewHandler} currentView={currentView} setCurrentView={setCurrentView}/>}
     </div>
